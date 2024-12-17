f10f38cbd9286d715866d6afecae994a1d7c981c
Shrey327 <shreyansh.pathak273@gmail.com>
2024-12-17 23:29:13 +0530

    added the meet the team page

--- CodeZero-frontend/src/App.jsx
+++ CodeZero-frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
+import Meet from './components/Meet'
 
 const App=() =>{
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-      
+        {/* <Navbar /> */}
+        <Meet />
       </BrowserRouter>
     </div>
   )
